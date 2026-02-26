f64c89172346c848787085f3c8fd05f8d64f4d42
V-Padmini <pvenkatesh3898@gmail.com>
2026-02-26 10:55:00 +0530

    First commit

--- js/model.js
+++ js/model.js
@@ -1,5 +1,9 @@
 const Model = {
-    todos: [],
+    todos: JSON.parse(localStorage.getItem('todos')) || [],
+
+    save() {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
 
     addTodo(text) {
         this.todos.push({
@@ -7,20 +11,28 @@ const Model = {
             text,
             completed: false
         });
+        this.save();
     },
 
     removeTodo(id) {
         this.todos = this.todos.filter(todo => todo.id !== id);
+        this.save();
     },
 
     toggleTodo(id) {
         const todo = this.todos.find(t => t.id === id);
-        if (todo) todo.completed = !todo.completed;
+        if (todo) {
+            todo.completed = !todo.completed;
+            this.save();
+        }
     },
 
     updateTodo(id, newText) {
         const todo = this.todos.find(t => t.id === id);
-        if (todo) todo.text = newText;
+        if (todo) {
+            todo.text = newText;
+            this.save();
+        }
     },
 
     getFilteredTodos(filter) {
